ba5cae762a759326f89e223196d9c60ff9f7be33
sai charan <S531499@nwmissouri.edu>
2019-02-08 15:00:21 -0600

    added new controller

---
+++ src/Web/Controllers/PuppyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class PuppyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Greeting()
+        {
+            return Content("Hello!");
+        }
+        public IActionResult JSON()
+        {
+            return Json(new
+            {
+                name = "Rocky",
+                since = "1951"
+            });
+        }
+        
+
+        public IActionResult HTML()
+        {
+            return Content("<!DOCTYPE html><html><body>Hello World</body></html>", "text/html");
+        }
+
+    }
+}
